57c6c68d6a1aac677a0d3de2551a8ad6acdd7d55
Edi <andyzimm@hotmail.com>
2018-06-10 10:59:53 +0200

    initial commit

---
+++ src/main/java/my/marketanalyzer/stockDataService/App.java
@@ -0,0 +1,13 @@
+package my.marketanalyzer.stockDataService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
